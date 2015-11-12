48b77d67bf8a7141a2d7f69dda84824d3b5fe6c4
Domingo Rosa <DomingoRosa@Domingos-MacBook-Air.local>
2015-11-12 09:28:25 -0800

    upload of updated assign 19

--- js/main.js
+++ js/main.js
@@ -13,54 +13,14 @@ var Contact = Backbone.Model.extend({
   idAttribute: "objectId"
 });
 
+  var Contacts = Backbone.Collection.extend({
+    model: Contact,
+    _parse_class_name: "Contact"
+  })
 
+  var ContactCollection = new Contacts();
 
- var Router = Backbone.Router.extend({
-  initialize: function () {
-    Backbone.history.start({pushState: true});
-  },
-
-  routes: {
-    "person/:objectId":"person",
-    "contact" : "contact",
-    "": "index"
-  }
-
-});
-
-var router = new Router();
-
-router.on("route:person", function(objectId) {
-  var person = new Contact({objectId: objectId});
-  person.fetch();
-  console.log(person);
-});
-
-router.on("route:contact", function () {
-  console.log("contact");
-});
-
-router.on("route:index", function () {
-  console.log("home page");
-});
-
-
-  $("body").on("click","a", function(e){
-  e.preventDefault();
-  var href = $(this).attr("href");
-  href = href.substr(1);
-  router.navigate(href, {trigger:true});
-  $("#listView").html("Couldn't figure out how to change templtes");
-});
-
- 
-var Contacts = Backbone.Collection.extend({
-  model: Contact,
-  _parse_class_name: "Contact"
-});
- var ContactCollection = new Contacts();
-
-    ContactCollection.fetch({
+  ContactCollection.fetch({
       success: function(resp) {
       var personObj = {"person" : resp.toJSON()};
       var nameTemplate = $("#nameTemplate").text();
@@ -72,6 +32,53 @@ var Contacts = Backbone.Collection.extend({
         }
   });
 
+  var Router = Backbone.Router.extend({
+    initialize: function(){
+      Backbone.history.start({pushState: true});
+    },
+    routes: {
+    "person/:objectId":"person",
+    "contact" : "contact",
+    "": "index"
+  }
+  });
+  var router = new Router();
 
+  router.on('route:person', function(objectId){
+    var person = new Contact({objectId: objectId});
+    person.fetch({
+      success: function(resp){
+      var personObj = {'person': resp.toJSON()};
+      var personTemplate = $("#personTemplate").text();
+      var personHTML = Mustache.render(personTemplate, personObj);
+        $("#listView").html(personHTML);
+        $("#names").hide();
+        $("#listView").show();
+      },error: function(err){
+        console.log("error ", err);
+      }
+  })
+  });
+
+  router.on('route:index', function(){
+    $("#listView").show();
+    $("#names").hide();
+  });
+
+  router.on("route:contact", function () {
+    console.log("contact");
+  });
+
+  router.on("route:index", function () {
+    console.log("home page");
+  });
+
+
+  $("body").on('click', 'a', function(e){
+    e.preventDefault();
+    var href = $(this).attr('href');
+    href = href.substr(1);
+    router.navigate(href,{trigger:true});
+  });
 });
 
