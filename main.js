c9e97cd6d2481aab34633064da32b2261f33c908
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-11-11 19:10:09 -0800

    Adds some js and minimal style change

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+$('a').on('click', function(e){
+  e.preventDefault();
+  var href = $(this).attr('href');
+  href = href.substr(1);
+  router.navigate(href, {trigger:true});
+});
+
 var Contact = Backbone.Model.extend({
   initialize: function() {
     },
@@ -25,8 +32,34 @@ ContactsCollection.fetch({
     var contactTemplate = $("#contactTemplate").text();
     var contactHTML = Mustache.render(contactTemplate, dataObj);
     $("#contactList").html(contactHTML);
+
+    var data2obj = {"data": resp.toJSON()};
+    var nameTemplate = $("#nameTemplate").text();
+    var nameHTML = Mustache.render(nameTemplate, data2obj);
+    $("#contacts").html(nameHTML);
+
     console.log("success: ", resp);
   }, error: function (err) {
     console.log("error: ", err);
   }
 });
+
+var Router = Backbone.Router.extend({
+  initialize: function() {
+    Backbone.history.start({pushState: true});
+  },
+  routes: {
+    "name/:objectId": "name",
+    "": "index"
+  }
+});
+
+var router = new Router();
+
+router.on('route:name', function(objectId) {
+  var name = new Contact({objectId: objectId});
+  name.fetch();
+  console.log(name);
+});
+
+
