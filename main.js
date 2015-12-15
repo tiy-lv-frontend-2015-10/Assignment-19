e3530645ca0c5a287fc963605fd26d001d9774e3
Eijah Duane Prince <elijahprince73@gmail.com>
2015-12-15 10:44:05 -0800

    fixed errors

--- main.js
+++ main.js
@@ -1,83 +1,87 @@
-$(document).ready(function(){
-var Contacts = Backbone.Model.extend({
-    initialize: function () {
-      console.log ("A new list has been created");
-      },
-      defaults:{
-        Name: null,
-        Email: null,
-        Location: null,
-        Number: null
-      },
-      _parse_class_name:"list"
-    });
-  
+$(document).ready(function () {
+	var Contacts = Backbone.Model.extend({
+		initialize: function () {
+			console.log("A new list has been created");
+		},
+		defaults: {
+			Name: null,
+			Email: null,
+			Location: null,
+			Number: null
+		},
+		_parse_class_name: "list"
+	});
 
-  var List = new Contacts();
 
-  var Lists = Backbone.Collection.extend({
-    model: Contacts,
-    _parse_class_name: "list"
-  });
+	var List = new Contacts();
 
-//Contact list
-  var ListsCollection = new Lists();
+	var Lists = Backbone.Collection.extend({
+		model: Contacts,
+		_parse_class_name: "list"
+	});
 
-   ListsCollection.fetch({
-    success: function(resp) {
-        var taco = {"meat":resp.toJSON()};
-        var mexicoTemplate = $("#mexicoTemplate").text();
-        var mexicoHTML = Mustache.render(mexicoTemplate,taco);           
-		$("#main").html(mexicoHTML);
-        console.log("success: ", resp);
-      }, error: function (err) {
-        console.log("error: ", err);
-      }
-    });
+	//Contact list
+	var ListsCollection = new Lists();
 
-    var Router = Backbone.Router.extend ({
-      initialize:function(){
-        Backbone.history.start({pushState:true});
-      },
-      routes:{
-        "name/:objectId":"name",
-        "":"index"
-      }
-    });  
+	ListsCollection.fetch({
+		success: function (resp) {
+			var taco = {
+				"meat": resp.toJSON()
+			};
+			var mexicoTemplate = $("#mexicoTemplate").text();
+			var mexicoHTML = Mustache.render(mexicoTemplate, taco);
+			$("#main").html(mexicoHTML);
+			console.log("success: ", resp);
+		},
+		error: function (err) {
+			console.log("error: ", err);
+		}
+	});
 
-//individual info
-    var router = new Router();
-    router.on('route:name', function(objectId){
-    var person = new Contacts({objectId:objectId});
-    person.fetch({success: function(resp){
-  var personInfo = {'persons': resp.toJSON()};
-  var personTemplate = $("#personTemplate").text();
-  var personHTML = Mustache.render(personTemplate , personInfo);
-  $("#info").html(personHTML);
-		  $("#main").hide();
-		  $("#info").show();
-      },error:function(err){
-        console.log("error " , err);
-      }
-    })
-    });
-	router.on('route:index' , function(){
+	var Router = Backbone.Router.extend({
+		initialize: function () {
+			Backbone.history.start({
+				pushState: true
+			});
+		},
+		routes: {
+			"name/:objectId": "name",
+			"": "index"
+		}
+	});
+
+	//individual info
+	var router = new Router();
+	router.on('route:name', function (objectId) {
+		var person = new Contacts({
+			objectId: objectId
+		});
+		person.fetch({
+			success: function (resp) {
+				var personInfo = {
+					'persons': resp.toJSON()
+				};
+				var personTemplate = $("#personTemplate").text();
+				var personHTML = Mustache.render(personTemplate, personInfo);
+				$("#info").html(personHTML);
+				$("#main").hide();
+				$("#info").show();
+			},
+			error: function (err) {
+				console.log("error ", err);
+			}
+		})
+	});
+	router.on('route:index', function () {
 		$("#main").show();
 		$("#info").hide();
 	});
-  $("body").on('click', 'a', function(e){
-    e.preventDefault();
-    var href = $(this).attr('href');
-    href = href.substr(1);
-    router.navigate(href,{trigger:true});
-  });
+	$("body").on('click', 'a', function (e) {
+		e.preventDefault();
+		var href = $(this).attr('href');
+		href = href.substr(1);
+		router.navigate(href, {
+			trigger: true
+		});
+	});
 });
-
-
-
-
-
-
-
-
-
